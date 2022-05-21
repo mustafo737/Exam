3e901cdca64d17aadc36bc878a41b5bff441a0f6
asadbektuit <davlatovasadbek1@gmail.com>
2022-05-21 20:50:29 +0500

    salom bro

--- src/main/java/com/example/exam/controller/DoctorController.java
+++ src/main/java/com/example/exam/controller/DoctorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 //salom asadbek
-
+//salom bro
 @RestController
 @RequestMapping("/doctor")
 public class DoctorController {
